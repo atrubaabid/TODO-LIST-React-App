55205f60df273e3a97ed34dc75e68448dc6e1169
atrubaabid <atrubaabid7@gmail.com>
2026-05-20 20:29:38 +0500

    todo list complete app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+
+  let [todolist, setTodolist] = useState([])
+
+
+  let saveTodoList = (event) => {
+    let toname = event.target.toname.value;
+
+    if (!todolist.includes(toname)) {
+      let finalTodoList = [...todolist, toname]
+      setTodolist(finalTodoList)
+
+    } else {
+      alert("This name already exists.....")
+    }
+
+    event.preventDefault();
+
+  }
+  let list = todolist.map((value, i) => {
+    return (
+      <TodoListItems value={value} key={i} indexNumber={i}
+        todolist={todolist} setTodolist={setTodolist}></TodoListItems>
+
+    )
+  })
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>TODO LIST PROJECT</h1>
+        <form onSubmit={saveTodoList}>
+          <input type='text' name='toname'></input> <button>Save</button>
+        </form>
+
+        <div className='outerDiv'>
+          <ul>
+            {list}
+          </ul>
+        </div>
+
+
+
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+function TodoListItems({ value, setTodolist, indexNumber, todolist }) {
+
+  let deleteRow = () => {
+    let finalData = todolist.filter((v, i) => i != indexNumber)
+    setTodolist(finalData);
+  }
+
+
+  let [status, setstatus] = useState(false)
+
+  return (
+    <li className={status ? 'completetodo' : ''} onClick={() => setstatus(!status)}>{indexNumber + 1} {value} <span onClick={deleteRow}>&times;</span></li>
+
+  )
+}
